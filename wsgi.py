ca2fe97db8ec8bb73be41c81d38cb339eb533dab
= <=>
2018-10-31 20:17:06 -0400

    Trying again.

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
 from server import application
 
 if __name__ == "__main__":
-    from gevent.pywsgi import WSGIServer
-    
-    http_server = WSGIServer(('', 8000), application)
-    http_server.serve_forever()
+    application.run()
